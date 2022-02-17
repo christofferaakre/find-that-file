d6e37c7c88446241af436b4a81e111464633a35d
Christoffer Aakre <christoffer.corfield@gmail.com>
2022-02-17 11:43:43 +0100

    make open_file cross platform

--- utils.py
+++ utils.py
@@ -1,12 +1,40 @@
+from platform import win32_edition
+import os
 import sys
 import pathlib
 from pathlib import Path
 import subprocess
 
+def get_platform():
+    if sys.platform == 'linux':
+        try:
+            proc_version = open('/proc/version').read()
+            if 'Microsoft' in proc_version:
+                return 'wsl'
+        except:
+            pass
+
+    return sys.platform
+
 def open_file(path: Path):
-    platform = sys.platform
-    if platform == 'linux':
-        args = ["xdg-open", str(path)]
-        proc = subprocess.run(args)
+    filename = str(path)
+    platform = get_platform()
+
+    # macos
+    if platform == 'darwin':
+        args = ['open', filename]
+        subprocess.run(args)
+
+    # windows
+    elif platform in ['win32', 'win64']:
+        os.startfile(filename.replace('/', '\\'))
+
+    # windows WSL
+    elif platform == 'wsl':
+        subprocess.call('cmd.exe /C start'.split() + [filename])
+
+    # linux
     else:
-        raise NotImplementedError
+        args = ['xdg-open', filename]
+        subprocess.call(args)
+
